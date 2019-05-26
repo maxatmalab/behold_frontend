e97e29613dd07821acb44ed4b67ecb26a4fe7a3f
Pavel Nenashev <nps-11@mail.ru>
2019-05-26 15:07:43 +0200

    good

---
+++ screens/TeachersScreen.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+
+type Props = {
+
+}
+
+class TeachersScreen extends Component<Props> {
+  <View>
+      
+  </View>
+}
